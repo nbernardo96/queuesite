130c12d567e2e06b0ac994355db807b6ec862fc2
Nicole <nicolebernardo96@gmail.com>
2020-09-24 17:55:32 -0700

    added flyer and businesscard routes and pages

--- src/containers/Flyer.js
+++ src/containers/Flyer.js
@@ -15,7 +15,10 @@ import {
     Sidebar,
     Visibility,
     Dropdown,
-    Modal
+    Modal,
+    Checkbox,
+    Form,
+    Label
 } from "semantic-ui-react";
 
 import flyer from './images/flyer-img.png';
@@ -97,95 +100,63 @@ ResponsiveContainer.propTypes = {
     children: PropTypes.node
 };
 
-function HandleDropdown(props) {
-    return <h1>You chose, {props.option}</h1>;
-}
-
-const HomepageLayout = () => (
+const FlyerPage = () => (
     < ResponsiveContainer >
-        <Segment style={{ padding: "8em 0em" }} vertical>
+        <Segment style={{ padding: "8em 0em 5em 0em" }} vertical>
             <Grid container stackable verticalAlign="middle">
                 <Grid.Row>
                     <Grid.Column width={8} textAlign="center">
                         <Header as="h1"
                             style={{ fontSize: "2.5em" }} >
-                            Sunshine Life & Health Advisors {" "}
+                            Flyer Form {" "}
                         </Header>{" "}
-                        <p style={{ fontSize: "1.33em" }} > </p>{" "} <Header as="h3" style={{ fontSize: "1.50em" }} > We Make Personalized Business Cards and Flyers {" "}
-                        </Header>{" "}
-                        <p style={{ fontSize: "1.20em" }} > Choose and fill out an order form below {" "} </p>{" "}
-
-                    </Grid.Column>{" "}
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column textAlign="center">
-                        <Modal trigger={<Button size="large">Fill Out Form</Button>} closeIcon>
-                            <Modal.Header>
-                                Choose A Form
-                            </Modal.Header>
-                            <Modal.Content Dropdown>
-                                <Dropdown
-                                    placeholder='Select choice'
-                                    fluid
-                                    compact
-                                    selection
-                                    options={options} />{"\n"}
-                            </Modal.Content>
-                            <Modal.Content>
-                                <Button>Select</Button>
-                            </Modal.Content>
-                        </Modal>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
-        <Segment style={{ padding: "0em" }} vertical>
-            <Grid celled="internally" columns="equal" stackable>
-                <Grid.Row textAlign="center">
-                    <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                        <Header as="h3" style={{ fontSize: "2em" }}>
-                            Flyers
-                        </Header>
                         <Image
                             centered
                             rounded
                             size="medium"
                             src={flyer}
                         />
-                    </Grid.Column>
-                    <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                        <Header as="h3" style={{ fontSize: "2em" }}>
-                            Business Cards
-                        </Header>
-                        <Image
-                            centered
-                            size="large"
-                            src={business_card_1}
-                        />
-                        <Image
-                            centered
-                            size="large"
-                            src={business_card_2}
-                        />
-                    </Grid.Column>
+                        <p style={{ fontSize: "1.33em" }} > </p>{" "}
+                    </Grid.Column>{" "}
                 </Grid.Row>
             </Grid>
+        </Segment>
+        <Segment style={{ padding: "5em 0em" }} vertical>
+
+            <Container>
+                <Form>
+                    <Form.Field>
+                        <label style={{ fontSize: "20px" }}>Agent Name</label>
+                        <input placeholder='Agent Name' />
+                    </Form.Field>
+                    <Form.Field>
+                        <label style={{ fontSize: "20px" }}>Phone</label>
+                        <input placeholder='Phone' />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox label='I agree to the Terms and Conditions' />
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+            </Container>
+
+
         </Segment>
         <Segment style={{ padding: "8em 0em" }} vertical>
             <Container text textAlign="center">
                 <Header as="h3" style={{ fontSize: "2em" }}>
                     Tony's Design Queue
-                </Header>
+            </Header>
                 <Header as="h4" style={{ fontSize: "2em" }}>
                     El queue de diseño para Tony
-                </Header>
+            </Header>
                 <p style={{ fontSize: "1.33em" }}>
                     Please, only use this when necessary.
-                    <p>Por favor, utiliza esto cuando sea necessario.</p>
+                <p>Por favor, utiliza esto cuando sea necessario.</p>
                 </p>
             </Container>
         </Segment>
     </ResponsiveContainer >
 );
 
-export default HomepageLayout;
+export default FlyerPage;
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Hoc from "./hoc/hoc";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import HomepageLayout from "./containers/Home";
+import FlyerPage from "./containers/Flyer";
+import BusinessCardPage from "./containers/BusinessCard";
 
 
 const BaseRouter = () => (
@@ -12,6 +14,9 @@ const BaseRouter = () => (
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/" component={HomepageLayout} />
+        <Route path="/flyer" component={FlyerPage} />
+        <Route path="/businesscard" component={BusinessCardPage} />
+
     </Hoc>
 );
 
